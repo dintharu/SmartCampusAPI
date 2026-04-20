351140925765842a7fdb81e9059cad5c074ca861
dintharu <Laknathoki@gmail.com>
2026-04-21 04:58:05 +0530

    feat(exception): add RoomNotEmptyException for deletion conflicts

---
+++ src/main/java/com/smartcampus/w2120049_okithadintharu_5cosc022w/exception/RoomNotEmptyException.java
@@ -0,0 +1,8 @@
+package com.smartcampus.w2120049_okithadintharu_5cosc022w.exception;
+
+public class RoomNotEmptyException extends RuntimeException {
+
+    public RoomNotEmptyException(String message) {
+        super(message);
+    }
+}
