24e9ec35a0dc1f88bfe39eda09a7ee362a2af691
dintharu <Laknathoki@gmail.com>
2026-04-21 04:42:08 +0530

    feat(exception): add ResourceNotFoundException for 404 errors

---
+++ src/main/java/com/smartcampus/w2120049_okithadintharu_5cosc022w/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.smartcampus.w2120049_okithadintharu_5cosc022w.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
