357718abf1a2d4578bc5f2f0775db01d423c74ae
dintharu <Laknathoki@gmail.com>
2026-04-21 05:34:41 +0530

    feat(exception): add LinkedResourceNotFoundException for orphan refs

---
+++ src/main/java/com/smartcampus/w2120049_okithadintharu_5cosc022w/exception/LinkedResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.smartcampus.w2120049_okithadintharu_5cosc022w.exception;
+
+
+public class LinkedResourceNotFoundException extends RuntimeException {
+
+    public LinkedResourceNotFoundException(String message) {
+        super(message);
+    }
+}
