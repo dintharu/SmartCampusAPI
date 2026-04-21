6afaf4202086b93733d6e4d41561a989c6e811d9
dintharu <Laknathoki@gmail.com>
2026-04-21 05:47:33 +0530

    feat(exception): add SensorUnavailableException for state violations

---
+++ src/main/java/com/smartcampus/w2120049_okithadintharu_5cosc022w/exception/SensorUnavailableException.java
@@ -0,0 +1,9 @@
+package com.smartcampus.w2120049_okithadintharu_5cosc022w.exception;
+
+
+public class SensorUnavailableException extends RuntimeException {
+
+    public SensorUnavailableException(String message) {
+        super(message);
+    }
+}
